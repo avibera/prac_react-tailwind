522d9cae2d70fc12fac620a29128a32653e4367d
avinash <avinash.b@mindflur.com>
2023-09-24 09:02:55 +0530

    Comit the change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import FormTab from './components/FormTab';
-import SuccessToast from './components/SuccessToast';
 import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
     <>
     <FormTab/>
-    <SuccessToast/>
     <ToastContainer/>
     </>
   );
